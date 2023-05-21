eafd59aeb364b6079ab8525b8d76ecd1dab57609
Fabrizio <farfanfabrizio96@gmail.com>
2023-05-21 08:44:28 +0200

    Commit on FixingBugs branch

--- app/src/main/java/com/example/basiccoroutineexample/MainActivity.kt
+++ app/src/main/java/com/example/basiccoroutineexample/MainActivity.kt
@@ -2,36 +2,55 @@ package com.example.basiccoroutineexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.example.basiccoroutineexample.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var job: Job? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.addButton.setOnClickListener {
-            val number = binding.numberAddedET.text.toString().toInt()
-            CoroutineScope(Main).launch{
-                addNumber(number)
-            }
+                if(job!=null){
+                    job!!.cancel()
+                }
+                lifecycleScope.launch(Main) {
+                    addNumber()
+
+                }
+
         }
 
     }
 
+    private suspend fun addNumber() {
 
-    private suspend fun addNumber(number: Int){
-        binding.numberAddedET.text.clear()
-        delay(2000L)
-        binding.resultET.text = binding.resultET.text.toString().toInt().plus(number).toString()
+        val numberEditText = binding.numberAddedET
+        var resultTextView = binding.resultET
+
+        var numberToIncrease = numberEditText.text.toString().toIntOrNull()
+
+        resultTextView.text = numberToIncrease.toString()
+
+        if (numberToIncrease != null) {
+           job = CoroutineScope(Main).launch {
+
+                while (isActive) {
+                    delay(2000)
+                    numberToIncrease++
+                    resultTextView.text = numberToIncrease.toString()
+                }
+
+            }
+            job!!.join()
+        } else { resultTextView.text = "Insert a Number" }
+
     }
 
 }
