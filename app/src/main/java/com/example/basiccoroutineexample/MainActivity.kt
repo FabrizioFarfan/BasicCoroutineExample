c8c9dc443199e1b842fd56645f5e65e253aeb0e1
FabrizioFarfan <112548251+FabrizioFarfan@users.noreply.github.com>
2023-06-01 11:11:48 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/basiccoroutineexample/MainActivity.kt
+++ app/src/main/java/com/example/basiccoroutineexample/MainActivity.kt
@@ -10,45 +10,68 @@ import kotlinx.coroutines.Dispatchers.Main
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var job: Job? = null
+    private var firstTime = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.addButton.setOnClickListener {
-                if(job!=null){
-                    job!!.cancel()
-                }
-                lifecycleScope.launch(Main) {
-                    addNumber()
 
+
+
+            if (!firstTime){
+                lifecycleScope.launch(Main) {
+                    addOneFirstTime()
                 }
+            } else {
+                lifecycleScope.launch(Main) {
+                    addOneMoreTimes()
+                }
+            }
 
         }
 
     }
 
-    private suspend fun addNumber() {
+    private suspend fun addOneFirstTime() {
 
         val numberEditText = binding.numberAddedET
-        var resultTextView = binding.resultET
+        val resultTextView = binding.resultET
 
         var numberToIncrease = numberEditText.text.toString().toIntOrNull()
 
         resultTextView.text = numberToIncrease.toString()
 
         if (numberToIncrease != null) {
-           job = CoroutineScope(Main).launch {
 
-                    delay(2000)
-                    numberToIncrease++
-                    resultTextView.text = numberToIncrease.toString()
-          
+           val newNumber = CoroutineScope(Main).async {
 
+                delay(2000)
+                numberToIncrease++
+                firstTime = true
+                resultTextView.text = numberToIncrease.toString()
             }
-            job!!.join()
+            newNumber.await()
+
         } else { resultTextView.text = "Insert a Number" }
+
+    }
+
+    private suspend fun addOneMoreTimes(){
+
+        val resultTextView = binding.resultET
+        val resultTextViewValue = binding.resultET.text.toString().toInt()
+
+        val newNumber = CoroutineScope(Main).async {
+
+            delay(2000)
+
+            firstTime = true
+            resultTextView.text = resultTextViewValue.plus(1).toString()
+        }
+        newNumber.await()
 
     }
 
